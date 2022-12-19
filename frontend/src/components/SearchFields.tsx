4d65018faa66d11e59cbd710aa5a5491f1cc20e0
motoki317 <motoki317@gmail.com>
2022-12-19 20:35:39 +0900

    multiple type fields

--- frontend/src/components/SearchFields.tsx
+++ frontend/src/components/SearchFields.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent, useEffect, useState} from "react";
-import {Button, Divider, FormControl, MenuItem, Select, TextField} from "@mui/material";
+import {Button, Checkbox, Divider, FormControl, ListItemText, MenuItem, Select, TextField} from "@mui/material";
 import {RefactoringType} from "../../../common/common";
 import {SearchField} from "./SearchField";
 
@@ -21,7 +21,7 @@ interface Props {
 export const SearchFields: FunctionComponent<Props> = ({className, query, setQuery, queryError}) => {
   const [rawField, setRawField] = useState(query)
 
-  const [type, setType] = useState('')
+  const [types, setTypes] = useState<string[]>([])
   const [commit, setCommit] = useState('')
   const [repository, setRepository] = useState('')
 
@@ -34,9 +34,13 @@ export const SearchFields: FunctionComponent<Props> = ({className, query, setQue
     setRawField(query)
   }, [setRawField, query])
 
-  const updateFromRichField = (typ: string = type) => {
+  const updateFromRichField = (t: string[] = types) => {
     const conditions = []
-    if (typ) conditions.push(`type = "${typ}"`)
+    if (t.length > 0) {
+      const clause = t.map((t) => `type = "${t}"`).join(" | ")
+      if (t.length === 1) conditions.push(clause)
+      else conditions.push(`(${clause})`)
+    }
     if (commit) conditions.push(`commit = ${commit}`)
     if (repository) conditions.push(`repository = ${repository}`)
     setQuery(conditions.join(" & "))
@@ -64,12 +68,23 @@ export const SearchFields: FunctionComponent<Props> = ({className, query, setQue
         <div className="flex flex-row flex-wrap grid-cols-2 gap-4">
           <div className="flex flex-row gap-2">
             <div className="flex-none my-auto">Type =</div>
-            <Select value={type} onChange={(e) => {
-              setType(e.target.value)
-              updateFromRichField(e.target.value)
-            }}>
-              <MenuItem value=""><em>None</em></MenuItem>
-              {Object.values(RefactoringType).map((refType) => <MenuItem key={refType} value={refType}>{refType}</MenuItem>)}
+            <Select
+              multiple
+              value={types}
+              renderValue={(s) => s.length === 1 ? s[0] : s.length === 0 ? '' : `${s.length} selected`}
+              onChange={(e) => {
+                const val = e.target.value
+                const types = typeof val === 'string' ? val.split(",") : val
+                setTypes(types)
+                updateFromRichField(types)
+              }}
+            >
+              {Object.values(RefactoringType).map((refType) => (
+                <MenuItem key={refType} value={refType}>
+                  <Checkbox checked={types.indexOf(refType) > -1} />
+                  <ListItemText primary={refType} />
+                </MenuItem>
+              ))}
             </Select>
           </div>
           <SearchField name="Commit" value={commit} setValue={setCommit} update={updateFromRichField} />
