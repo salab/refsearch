bccb9e1e9d8a0bb38e6e7405ae2552dc4e4131d6
motoki317 <motoki317@gmail.com>
2022-12-30 12:50:56 +0900

    add indexes

--- backend/src/cmd/index.ts
+++ backend/src/cmd/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import {registerRoutes} from "../api";
+import {createMissingIndexes} from "../mongo";
 
-const app = express()
-app.use(express.json())
-registerRoutes(app)
+const main = async () => {
+  await createMissingIndexes()
 
-const port: number = Number.parseInt(process.env.PORT ?? '') || 3000
-app.listen(port, () => console.log(`API server started on port ${port}`))
+  const app = express()
+  app.use(express.json())
+  registerRoutes(app)
+
+  const port: number = Number.parseInt(process.env.PORT ?? '') || 3000
+  app.listen(port, () => console.log(`API server started on port ${port}`))
+}
+
+main()
--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -1,6 +1,7 @@
-import {MongoClient} from "mongodb";
+import {Collection, CreateIndexesOptions, Document, IndexSpecification, MongoClient} from "mongodb";
 import {CommitMeta, RefactoringMeta, RepositoryMeta} from "../../common/common";
 import {Job} from "../../common/jobs";
+import {formatTime} from "../../common/utils";
 
 const env = {
   user: process.env.MONGODB_USER || 'root',
@@ -19,3 +20,39 @@ export const commitsCol = db.collection<CommitMeta>('commits')
 export const refCol = db.collection<RefactoringMeta>('refactorings')
 
 export const jobCol = db.collection<Job>('jobs')
+
+interface Index { name: string; key: IndexSpecification }
+type IndexDef = [spec: IndexSpecification, opt: CreateIndexesOptions]
+const createIndexes = async <T extends Document>(col: Collection<T>, defs: IndexDef[]) => {
+  const indexesCursor = col.listIndexes()
+  const indexes: Index[] = []
+  await indexesCursor.forEach((idx) => void indexes.push(idx as Index))
+
+  // Drop indexes
+  for (const { name } of indexes) {
+    if (name === '_id_') continue
+    if (defs.find(([_, opt]) => name === opt.name)) continue
+    const start = performance.now()
+    await col.dropIndex(name)
+    console.log(`[mongo.ts] Dropped index ${name} on ${col.collectionName} in ${formatTime(start)}`)
+  }
+  // Add indexes
+  for (const [spec, opt] of defs) {
+    if (indexes.find((idx) => idx.name === opt.name)) continue
+    const start = performance.now()
+    await col.createIndex(spec, opt)
+    console.log(`[mongo.ts] Added index ${opt.name} to ${col.collectionName} in ${formatTime(start)}`)
+  }
+}
+export const createMissingIndexes = async () => {
+  await createIndexes(commitsCol, [
+    [[['repository', 1]], { name: 'idx_repository' }],
+    [[['date', 1]], { name: 'idx_date' }],
+  ])
+  await createIndexes(refCol, [
+    [[['type', 1]], { name: 'idx_type' }],
+    [[['sha1', 1]], { name: 'idx_sha1' }],
+    [[['commit.date', 1]], { name: 'idx_commit_date' }],
+  ])
+  console.log(`[mongo.ts] Finished syncing indexes`)
+}
